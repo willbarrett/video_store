bda6b55cad6b1276cf6ee218086352ab5c560f36
Will Barrett <will@tallgreentree.com>
2013-06-28 10:02:34 -0500

    Editing of videos by admins works

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,5 +1,7 @@
 class Admin::VideosController < Admin::BaseController
 
+  before_filter :find_video, only: [:edit, :update]
+
   def new
     @video = Video.new
   end
@@ -13,5 +15,21 @@ class Admin::VideosController < Admin::BaseController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @video.update_attributes(params[:video])
+      redirect_to admin_root_url, notice: 'Video Updated'
+    else
+      render action: 'edit'
+    end
+  end
+
+  protected
+  
+  def find_video
+    @video = Video.find_by_slug!(params[:id])
+  end
 
 end
